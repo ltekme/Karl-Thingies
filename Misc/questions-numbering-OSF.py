61d5db4e5d07fb1a070ca7a167193c3d25749a7c
Karl <karl@ltek.me>
2023-12-27 17:31:04 +0000

    import more things

---
+++ Misc/questions-numbering-OSF.py
@@ -0,0 +1,11 @@
+from os import path as op
+
+file = 'submission.txt'
+
+file = op.join(op.dirname(op.realpath(__file__)), file)
+open(file, 'w').close()
+file = open(file, 'r+')
+
+n    = 23
+for i in range(1, n+1):
+    file.write(f'[{i}]\n')
